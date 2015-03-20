df42a5e234fd649558216363d549f406ecacbd1d
hMatoba <hiroaki.mtb@outlook.com>
2015-03-21 01:27:34 +0900

    Update phestum.js.

--- phestum.js
+++ phestum.js
@@ -1,7 +1,5 @@
-
-
 var phestum = {
-    version : "0.1.0",
+    version : "0.2.0a",
     
     _isEqual : function (a, b) {
         if (typeof(a) !== typeof(b)) {
@@ -72,16 +70,16 @@ var phestum = {
 
 
 var Tests = {
-    pass: 0,
-    fail: 0,
     _exec: function () {
+        var pass = 0;
+        var fail = 0;
         var keys = Object.keys(Tests);
         var tests = keys.filter(function (item) {
             return (item[0] !== "_" &&
                     typeof(Tests[item]) === "function" &&
                     item.indexOf("Test") > -1);
         });
-        console.log(JSON.stringify(tests));
+        console.log("tests: " + JSON.stringify(tests));
 
         for (var p=0; p<tests.length; p++) {
             console.log("\n*************\n" + tests[p]);
@@ -90,18 +88,18 @@ var Tests = {
                 Tests[tests[p]]();
                 console.log("=============");
                 console.log("passed.");
-                Tests.pass += 1;
+                pass += 1;
             } catch (e) {
                 console.log("=============");
                 console.log(e);
                 console.log("failed.");
-                Tests.fail += 1;
+                fail += 1;
             }
         }
 
-        console.log("\n\nPassed: " + Tests.pass);
-        console.log("Failed: " + Tests.fail);
-        if (Tests.fail) {
+        console.log("\n\nPassed: " + pass);
+        console.log("Failed: " + fail);
+        if (fail) {
             return false;
         } else {
             return true;
@@ -109,58 +107,58 @@ var Tests = {
     },
 };
 
-(function () {
-    var system = require("system");
-    var fs = require("fs");
-    
-    // prepare tests
-    var testFiles = fs.list("tests/")
+
+// test process ++++++++++++++++++++++++++++++++++++
+var system = require("system");
+var fs = require("fs");
+
+// tests/files
+if (fs.isDirectory("tests/files")) {
+    Tests.files = {};
+    var optionFiles = fs.list("tests/files/")
             .filter(function(item) {
-                return item.indexOf(".js") !== -1;
+                return [".", ".."].indexOf(item) == -1;
             })
             .map(function (item) {
-                return 'tests/' + item;
+                return item;
             });
 
-    console.log(testFiles);
-    for (var p=0; p<testFiles.length; p++) {
-        eval(fs.read(testFiles[p]));
+    console.log("files: " + JSON.stringify(optionFiles));
+    for (var p=0; p<optionFiles.length; p++) {
+        Tests.files[optionFiles[p]] = fs.read("tests/files/" + optionFiles[p], {charset: "LATIN-1"});
     }
+}
 
+// prepare libraries
+Tests._libs = [];
+if (system.args.length > 1) {
+    var libFiles = system.args.slice(1);
+    console.log("lib: " + JSON.stringify(libFiles));
+    Tests._libs = libFiles.map(function (i) {
+        return fs.read(i);
+    });
+}
 
-    // prepare libiraries
-    if (system.args.length > 1) {
-        var libFiles = system.args.slice(1);
-        console.log("lib: " + JSON.stringify(libFiles));
-        for (var p=0; p<libFiles.length; p++) {
-            eval(fs.read(libFiles[p]));
-        }
-    }
+// prepare tests
+Tests._tests = fs.list("tests/")
+        .filter(function(item) {
+            return item.indexOf(".js") !== -1;
+        })
+        .map(function (item) {
+            return 'tests/' + item;
+        });
 
+// load libs and tests
+for (var p=0; p<Tests._libs.length; p++) {
+    eval(Tests._libs[p]);
+}
+for (var p=0; p<Tests._tests.length; p++) {
+    eval(fs.read(Tests._tests[p]));
+}
 
-    // files
-    var files = {};
-    if (fs.isDirectory("tests/files")) {
-        var optionFiles = fs.list("tests/files/")
-                .filter(function(item) {
-                    return [".", ".."].indexOf(item) == -1;
-                })
-                .map(function (item) {
-                    return item;
-                });
-
-        console.log(optionFiles);
-        for (var p=0; p<optionFiles.length; p++) {
-            files[optionFiles[p]] = fs.read("tests/files/" + optionFiles[p], {charset: "LATIN-1"});
-        }
-    }
-    Tests.files = files;
-
-
-    // run tests
-    if (Tests._exec()) {
-        phantom.exit();
-    } else {
-        phantom.exit(1);
-    }
-})();
+// run tests
+if (Tests._exec()) {
+    phantom.exit();
+} else {
+    phantom.exit(1);
+}
