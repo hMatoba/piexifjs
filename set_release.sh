67d6ac11fa88fda8f73ab90227974f387cb7effb
hMatoba <hiroaki.mtb@outlook.com>
2018-08-26 21:42:16 +0900

    v2.0.0a test

--- set_release.sh
+++ set_release.sh
@@ -2,5 +2,5 @@
 if [ "$TRAVIS_BRANCH" == "release*" ]; then
   git config --local user.name "${GITHUB_USERNAME}"
   git config --local user.email "${GITHUB_MYMAIL}"
-  git tag "$(date +'%Y%m%d%H%M%S')-$(git log --format=%h -1)"
+  git tag "$(git log --format=%s -1)"
 fi
