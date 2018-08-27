bf4b48088a93309b5307fead15efc848ab02be0f
hMatoba <hiroaki.mtb@outlook.com>
2018-08-28 00:02:22 +0900

    m

--- set_release.sh
+++
@@ -1,7 +0,0 @@
-# !/bin/sh -x
-if [ "$TRAVIS_BRANCH" == "release*" ]; then
-  git config --local user.name "${GITHUB_USERNAME}"
-  git config --local user.email "${GITHUB_MYMAIL}"
-  echo "${TRAVIS_COMMIT_MESSAGE}"
-  git tag "${TRAVIS_COMMIT_MESSAGE}"
-fi
