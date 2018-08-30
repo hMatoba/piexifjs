c1bf22b21d80c2c126b004da815d5d440e756e40
hMatoba <hiroaki.mtb@outlook.com>
2018-08-31 01:00:52 +0900

    m

--- tests/browser_tests/setup.js
+++ tests/browser_tests/setup.js
@@ -9,7 +9,7 @@ const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup');
 
 module.exports = async function() {
   console.log(chalk.green('Setup Puppeteer'));
-  const browser = await puppeteer.launch({args: ['--no-sandbox']});
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--headless', '--disable-gpu']});
   // This global is not available inside tests but only in global teardown
   global.__BROWSER_GLOBAL__ = browser;
   // Instead, we expose the connection details via file system to be used in tests
