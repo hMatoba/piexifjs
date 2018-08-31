1a55ec2f6daf4502d92feb69f6723d7eb6659be2
hMatoba <hiroaki.mtb@outlook.com>
2018-08-31 23:47:52 +0900

    m

--- tests/browser_tests/piexifjs.test.js
+++ tests/browser_tests/piexifjs.test.js
@@ -23,8 +23,9 @@ describe(
     it('should be same output from load on node and browser ', async () => {
       const nodeOutput = nodePiexifjs.load(jpegBinary);
       console.log("before");
+      console.log(require.resolve("/root/repo/dist/piexifjs.js"));
       await page.addScriptTag({
-        path: require.resolve("../../dist/piexifjs")
+        path: require.resolve("/root/repo/dist/piexifjs.js")
       });
       
       console.log("after");
