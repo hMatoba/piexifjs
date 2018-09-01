3eddce28ea7196817e646d0c4cd905d9d510d6f1
hMatoba <hiroaki.mtb@outlook.com>
2018-09-01 15:11:36 +0900

    implement "insert" test

--- tests/node_tests/piexifjs.test.js
+++ tests/node_tests/piexifjs.test.js
@@ -1,4 +1,6 @@
 const fs = require("fs");
+const jpeg = require('jpeg-js');
+
 const piexifjs = require('../../dist/piexifjs');
 
 test('"load" returns a object contains IFD -- 1', () => {
@@ -63,5 +65,7 @@ test('success insert -- 1', () => {
   const jpegBinary = fs.readFileSync("./tests/files/noexif.jpg").toString("binary");
   const exifBinary = 'Exif\x00\x00MM\x00*\x00\x00\x00\x08\x00\x02\x01\x00\x00\x04\x00\x00\x00\x01\x00\x00\x00\n\x01\x01\x00\x04\x00\x00\x00\x01\x00\x00\x00\n\x00\x00\x00\x00';
   const jpegBinaryExifInsert = piexifjs.insert(exifBinary, jpegBinary);
-  throw new Error('Not "expect"ed output. Implement!');
+  const buffer = Buffer.from(jpegBinaryExifInsert, 'ascii');
+  jpeg.decode(buffer, true);
+  expect(jpegBinaryExifInsert).toMatch(exifBinary);
 });
