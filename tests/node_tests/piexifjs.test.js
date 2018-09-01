158979e804cba2986cd066e92757ffdbd098ff04
hMatoba <hiroaki.mtb@outlook.com>
2018-09-01 14:19:58 +0900

    Add tests

--- tests/node_tests/piexifjs.test.js
+++ tests/node_tests/piexifjs.test.js
@@ -1,13 +1,37 @@
 const fs = require("fs");
 const piexifjs = require('../../dist/piexifjs');
 
-test('load returns a object contains IFD - 1', () => {
+test('"load" returns a object contains IFD -- 1', () => {
   const jpegBinary = fs.readFileSync("./tests/files/r_canon.jpg").toString("binary");
   const exifObj = piexifjs.load(jpegBinary);
   expect(Object.keys(exifObj)).toContain('0th');
 });
 
-test('round trip "load" and "dump" - 1', () => {
+test('"dump" returns correct value" -- 1', () => {
+  const exifObj = {
+    '0th': {
+      256: 10,
+      257:10
+    }
+  };
+  const exifBinary = piexifjs.dump(exifObj);
+  const correctBinary = 'Exif\x00\x00MM\x00*\x00\x00\x00\x08\x00\x02\x01\x00\x00\x04\x00\x00\x00\x01\x00\x00\x00\n\x01\x01\x00\x04\x00\x00\x00\x01\x00\x00\x00\n\x00\x00\x00\x00';
+  expect(exifBinary).toBe(correctBinary);
+});
+
+test('"load" returns correct value" -- 1', () => {
+  const exifBinary = 'Exif\x00\x00MM\x00*\x00\x00\x00\x08\x00\x02\x01\x00\x00\x04\x00\x00\x00\x01\x00\x00\x00\n\x01\x01\x00\x04\x00\x00\x00\x01\x00\x00\x00\n\x00\x00\x00\x00';
+  const correctObj = {
+    '0th': {
+      256: 10,
+      257:10
+    }
+  };
+  const exifObj = piexifjs.load(exifBinary);
+  expect(exifObj).toEqual(correctObj);
+});
+
+test('round trip "load" and "dump" -- 1', () => {
   const jpegBinary = fs.readFileSync("./tests/files/r_sony.jpg").toString("binary");
   const exifObj1 = piexifjs.load(jpegBinary);
   const exifBinary = piexifjs.dump(exifObj1);
@@ -26,7 +50,7 @@ test('round trip "load" and "dump" - 1', () => {
   expect(exifObj1).toEqual(exifObj2);
 });
 
-test('success remove - 1', () => {
+test('success remove -- 1', () => {
   const jpegBinary = fs.readFileSync("./tests/files/r_pana.jpg").toString("binary");
   const exifObj = piexifjs.load(jpegBinary);
   expect(Object.keys(exifObj)).toContain('0th');
@@ -35,18 +59,9 @@ test('success remove - 1', () => {
   expect(exifObjRemovedExif).toEqual({});
 });
 
-test('success insert - 1', () => {
+test('success insert -- 1', () => {
   const jpegBinary = fs.readFileSync("./tests/files/noexif.jpg").toString("binary");
-  const exifObj = piexifjs.load(jpegBinary);
-  expect(Object.keys(exifObj)).not.toContain('Exif');
-  const exif = {
-    '0th': {
-      '256': 10,
-      '257': 10
-    }
-  };
-  const exifBinary = piexifjs.dump(exif);
+  const exifBinary = 'Exif\x00\x00MM\x00*\x00\x00\x00\x08\x00\x02\x01\x00\x00\x04\x00\x00\x00\x01\x00\x00\x00\n\x01\x01\x00\x04\x00\x00\x00\x01\x00\x00\x00\n\x00\x00\x00\x00';
   const jpegBinaryExifInsert = piexifjs.insert(exifBinary, jpegBinary);
-  const newExifObj = piexifjs.load(jpegBinaryExifInsert);
-  expect(newExifObj).toEqual(exif);
+  throw new Error('Not "expect"ed output. Implement!');
 });
