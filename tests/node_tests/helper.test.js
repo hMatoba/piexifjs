a659a082bcba81803e75a15009422620982f315e
Joachim Holwech <joachim.holwech@gmail.com>
2019-10-23 13:33:23 +0200

    Updated tests

--- tests/node_tests/helper.test.js
+++ tests/node_tests/helper.test.js
@@ -2,27 +2,27 @@
 const piexif = require('../../dist/piexif');
 
 test('test "degToDmsRational" -- 1', () => {
-  const rationalArray = piexif.helper.GPSHelper.degToDmsRational(180.0);
+  const rationalArray = piexif.GPSHelper.degToDmsRational(180.0);
   expect(rationalArray).toEqual([[180, 1], [0, 1], [0, 100]]);
 });
 
 test('test "degToDmsRational" -- 2', () => {
-  const rationalArray = piexif.helper.GPSHelper.degToDmsRational(30.303);
+  const rationalArray = piexif.GPSHelper.degToDmsRational(30.303);
   expect(rationalArray).toEqual([[30, 1], [18, 1], [1080, 100]]);
 });
 
 
 test('test "dmsRationalToDeg" -- 1', () => {
-  const deg = piexif.helper.GPSHelper.dmsRationalToDeg([[180, 1], [0, 1], [0, 100]], "N");
+  const deg = piexif.GPSHelper.dmsRationalToDeg([[180, 1], [0, 1], [0, 100]], "N");
   expect(deg).toBe(180.0);
 });
 
 test('test "dmsRationalToDeg" -- 2', () => {
-  const deg = piexif.helper.GPSHelper.dmsRationalToDeg([[30, 1], [18, 1], [1080, 100]], "N");
+  const deg = piexif.GPSHelper.dmsRationalToDeg([[30, 1], [18, 1], [1080, 100]], "N");
   expect(deg).toBe(30.303);
 });
 
 test('test "dmsRationalToDeg" -- 3', () => {
-  const deg = piexif.helper.GPSHelper.dmsRationalToDeg([[30, 1], [18, 1], [1080, 100]], "S");
+  const deg = piexif.GPSHelper.dmsRationalToDeg([[30, 1], [18, 1], [1080, 100]], "S");
   expect(deg).toBe(-30.303);
 });
